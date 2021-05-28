7331db865b978fe8ad87dde2e61f23bfafbeb639
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2021-05-28 13:35:59 +0530

    Profile Image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileImageComponent } from './profile-image/profile-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileImageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/profile-image/profile-image.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-image',
+  templateUrl: './profile-image.component.html',
+  styleUrls: ['./profile-image.component.scss']
+})
+export class ProfileImageComponent {
+}
