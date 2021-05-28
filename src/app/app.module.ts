b76e4119b1756a804af7a49528894097d56e21c8
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2021-05-28 15:46:33 +0530

    Added my interests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileImageComponent } from './profile-image/profile-image.component';
+import { MyInterestComponent } from './my-interest/my-interest.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileImageComponent
+    ProfileImageComponent,
+    MyInterestComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-interest/my-interest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-interest',
+  templateUrl: './my-interest.component.html',
+  styleUrls: ['./my-interest.component.scss']
+})
+export class MyInterestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
